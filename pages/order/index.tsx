252bae879fc84c6b9379abfa9fd9b41f932b5897
clausen0 <sancla@live.no>
2021-12-05 22:26:08 +0100

    jobbet med valg av maskin

--- pages/order/index.tsx
+++ pages/order/index.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import style from '@styles/Order/Choose.module.css';
 
 function Machines() {
     return (
-        <div>
-
+        <div className={style.container}>
+            <div className={style.label}>
+                <p className={style.labeltx}>Velg maskin</p>
+            </div>
+            <div className={style.undersqare}>
+                <button className={style.maskin}></button>
+                <button className={style.maskin}></button>
+                <button className={style.maskin}></button>
+                <button className={style.maskin}></button>
+            </div>
         </div>
     )
 };
