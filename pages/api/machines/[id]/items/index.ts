9c686bc8be9a0635bb028e9e780fb612ea214093
proff001 <thommhole@gmail.com>
2021-12-09 11:12:05 +0100

    Fiksa api routene

---
+++ pages/api/machines/[id]/items/index.ts
@@ -0,0 +1,20 @@
+import { NextApiHandler } from "next";
+import { query } from "@lib/db";
+
+const delay = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
+
+const handler: NextApiHandler = async(req, res) => {
+    const { id } = req.query;
+
+    if(!id) return res.status(400).json({ message: '`id` required' });
+
+    try {
+        const result = await query(`SELECT * FROM machine_items WHERE machineId = ?`, id);
+        await delay(5000);
+        return res.json(result);
+    } catch(e) {
+        if(e instanceof Error) res.status(500).json({ message: e.message });
+    };
+};
+
+export default handler;
