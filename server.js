1f619ec57e392fb822dee3e7b8a23d82bd93de1e
proff001 <thommhole@gmail.com>
2022-01-07 13:07:14 +0100

    En liten endring i server.js

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app
 	.then(() => {
 		createServer((req, res) => {
 			const parsedUrl = parse(req.url, true);
-			const { pathname, query } = parsedUrl;
-			
 			handle(req, res, parsedUrl);
-			console.log("pathname", pathname);
 		}).listen(port, (err) => {
 			if(err) throw err;
 			console.log(`> Ready on http://localhost:${port}`);
